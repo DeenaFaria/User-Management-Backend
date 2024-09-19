9235f1c484f8ecc6b62e93ca0bf2985d1f72c3b5
Deena Faria <deenafaria@gmail.com>
2024-09-19 19:47:33 +0600

    Updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const WebSocket = require('ws');
 
-const port = process.env.PORT || 5000;
+
 
 // Initialize express app
 const app = express();
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MySQL
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST, // set this to Railway's MySQL host
   user: process.env.DB_USER, // set this to Railway's MySQL user
   password: process.env.DB_PASS, // set this to Railway's MySQL password
@@ -23,6 +23,11 @@ const connection = mysql.createConnection({
 db.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 // JWT secret key
